71898e59ae4913ef0ab20346a358be602abc145f
linhao liu <linhao.liu98@gmail.com>
2024-05-01 23:32:16 -0400

    navbar and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IntroComponent } from './intro/intro.component';
+import { ServiceComponent } from './service/service.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ShowcaseComponent } from './showcase/showcase.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'intro', component: IntroComponent },
+  { path: 'service', component: ServiceComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: 'showcase', component: ShowcaseComponent },
+  { path: '**', redirectTo: 'intro' },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { ServiceComponent } from './service/service.component';
+import { ShowcaseComponent } from './showcase/showcase.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    ServiceComponent,
+    ShowcaseComponent,
+    ContactUsComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
